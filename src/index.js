89f10baccd002a61ced9dd10b40f32cc5d06335b
Alan Cinsavich <alan.cimsavich@gmail.com>
2021-07-26 12:28:19 -0400

    Completed Assingment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  const formBox = document.querySelector('#create-task-form' )
+  const taskList = document.querySelector('#task')
+
+  formBox.addEventListener('submit', function(event) {
+    event.preventDefault()
+
+    const taskInput = document.querySelector("#new-task-description").value;
+
+    const newTask = document.createElement('li');
+    newTask.innerText = taskInput
+
+    taskList.appendChild(newTask);
+
+
+  
+  });
+
+})
